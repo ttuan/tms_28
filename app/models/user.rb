92ccb09077ad79eb4dd6be9418f97741ec491715
ttuan <tuantv.nhnd@gmail.com>
2015-07-17 14:56:21 +0700

    Show user active course in user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :user_courses, dependent: :destroy
   has_many :user_subjects, dependent: :destroy
   has_many :user_tasks, dependent: :destroy
+  has_many :courses, through: :user_courses
 
   mount_uploader :avatar, AvatarUploader
 end
