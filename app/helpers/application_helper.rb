dc35a638b7cecd29e44bd43bea9a298e78599492
ttuan <tuantv.nhnd@gmail.com>
2015-07-22 09:06:47 +0700

    User ransack gem to search and sort user for admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,13 @@ module ApplicationHelper
     href = html_options[:href] || '#'
     content_tag :a, name, html_options.merge(href: href, onclick: onclick)
   end
+
+  def link_add_fields name, f, type
+    new_object = f.object.send "build_#{type}"
+    id = "new_#{type}"
+    fields = f.send("#{type}_fields", new_object, child_index: id) do |builder|
+      render(type.to_s + "_fields", f: builder)
+    end
+    link_to(name, "#", class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  end
 end
